7a08a79c225ed6d5ff9f69331c89405c5fed3faa
张锋 <zhangfeng@zhangfengdeMacBook-Pro.local>
2017-05-24 19:32:50 +0800

    forget to add important files

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const Koa = require('koa');
-const app = new Koa();
+var serve = require('koa-static');
+var koa = require('koa');
+var app = new koa();
 
-// response
-app.use(ctx => {
-    ctx.body = '你好啊，这里是张锋的memo';
-});
+// $ GET /package.json
+app.use(serve('./public'));
+
+// $ GET /hello.txt
+
 
 app.listen(3000);
+
+console.log('listening on port 3000');
